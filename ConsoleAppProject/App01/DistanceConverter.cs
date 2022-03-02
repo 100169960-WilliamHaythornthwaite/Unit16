d5aa96d4bf73c7c95ff9e314cdd9b0156bcd9a7e
William Haythornthwaite <100169960@student.burnley.ac.uk>
2022-03-02 14:24:37 +0000

    Code for InputMiles

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -1,4 +1,5 @@
-﻿namespace ConsoleAppProject.App01
+﻿using System;
+namespace ConsoleAppProject.App01
 {
     /// <summary>
     /// Please describe the main features of this App
@@ -9,31 +10,34 @@
     public class DistanceConverter
     {
         private double miles;
-
         private double feet;
 
         /// <summary>
         /// 
         /// </summary>
         public void Run()
-            {
-            
-            }
+        {
+            InputMiles();
+            CalculateFeet();
+            OutputFeet();
+        }
         /// <summary>
         /// Prompt the user to put distance in miles
         /// Input the miles as a double number
         /// </summary>
         private void InputMiles()
-            {
+        {
+            Console.Write("Please enter the number of miles >");
+            string value = Console.ReadLine();
+            miles = Convert.ToDouble(value);
+        }
+        private void CalculateFeet()
+        {
 
-            }
-        private void InputFeet()
-            {
-
-            }
+        }
         private void OutputFeet()
-            {
+        {
             
-            }
+        }
     }
 }
