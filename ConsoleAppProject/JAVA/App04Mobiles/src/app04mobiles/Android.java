b05414735acb228d8969082d635242a741d5a267
William Haythornthwaite <100169960@student.burnley.ac.uk>
2022-03-23 15:59:32 +0000

    Start on App04

---
+++ ConsoleAppProject/JAVA/App04Mobiles/src/app04mobiles/Android.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app04mobiles;
+
+/**
+ *
+ * @author 100169960
+ */
+public class Android {
+    String Android_Version;
+    
+}
---
+++ ConsoleAppProject/JAVA/App04Mobiles/src/app04mobiles/Mobiles.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package app04mobiles;
+
+/**
+ *
+ * @author 100169960
+ */
+public class Mobiles {
+    int cost;
+    String colour;
+    String RAM;
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public String getRAM() {
+        return RAM;
+    }
+
+    public void setRAM(String RAM) {
+        this.RAM = RAM;
+    }
+    
+}
