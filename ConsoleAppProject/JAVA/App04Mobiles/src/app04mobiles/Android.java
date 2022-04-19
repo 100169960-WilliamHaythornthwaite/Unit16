a1e8d0d4351b58ef5843237cb7e881757869ffd9
William Haythornthwaite <100169960@student.burnley.ac.uk>
2022-04-19 11:37:57 +0100

    Getting my stuff

--- ConsoleAppProject/JAVA/App04Mobiles/src/app04mobiles/Android.java
+++ ConsoleAppProject/JAVA/App04Mobiles/src/app04mobiles/Android.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package app04mobiles;
 
@@ -8,7 +8,13 @@ package app04mobiles;
  *
  * @author 100169960
  */
-public class Android {
-    String Android_Version;
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
     
 }
