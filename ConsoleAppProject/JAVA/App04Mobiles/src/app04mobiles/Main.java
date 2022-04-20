b2752146708b457dd00fbe2e37a98ed0e64d9730
William Haythornthwaite <100169960@student.burnley.ac.uk>
2022-04-20 13:27:19 +0100

    Fixing

--- ConsoleAppProject/JAVA/App04Mobiles/src/app04mobiles/Main.java
+++ ConsoleAppProject/JAVA/App04Mobiles/src/app04mobiles/Main.java
@@ -15,6 +15,9 @@ public class Main {
      */
     public static void main(String[] args) {
         Samsung s = new Samsung();
+        Nokia n = new Nokia();
+        Xiaomi x = new Xiaomi();
+        
         s.setAndroid_Version("Android Pie");
         s.setColour("Black");
         s.setCost(3000);
@@ -24,6 +27,35 @@ public class Main {
         System.out.println("Colour: "+s.getColour());
         System.out.println("RAM: "+s.getRAM());
         System.out.println("Android version: "+s.getAndroid_Version());
+        
+        n.setColour("Silver");
+        n.setCost(40000);
+        n.setRAM("4GB");
+        n.setAndroid_Version("Andoid marshmelo");
+
+        x.setColour("Red");
+        x.setCost(50000);
+        x.setRAM("6GB");
+        x.setAndroid_Version("Andoid Kitkat");
+
+        System.out.println("Samsung Details: ");
+        System.out.println("Colour: "+s.getColour());
+        System.out.println("Cost: "+ s.getCost());
+        System.out.println("RAM"+ s.getRAM());
+        System.out.println("RAM: "+ s.getRAM());
+        System.out.println(" Samsung android version: " + s.getAndroid_Version());
+
+        System.out.println("Nokia Details: ");
+        System.out.println("Colour: "+n.getColour());
+        System.out.println("Cost: "+ n.getCost());
+        System.out.println("RAM: "+ n.getRAM());
+        System.out.println(" Nokia android version: " + n.getAndroid_Version());
+
+        System.out.println("Xiaomi Details: ");
+        System.out.println("Colour: "+x.getColour());
+        System.out.println("Cost: "+ x.getCost());
+        System.out.println("RAM: "+ x.getRAM());
+        System.out.println(" Xiaomi android version: " + x.getAndroid_Version());
     }
     
 }
